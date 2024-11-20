d3bb3521628f20f463e4a1d1b7f0fc206aa55369
Mateusz Cieplak <m.cieplak97@gmail.com>
2024-11-20 20:42:50 +0100

    Task solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -7,6 +7,22 @@
 
 function makeRobotAccountant() {
   // write code here
+  let count = 0;
+  const result = function getSum(a) {
+    return function(b) {
+      count++;
+
+      const number = count % 2;
+
+      if (count > 3 && number === 0) {
+        return 'Bzzz... Error!';
+      }
+
+      return a + b;
+    };
+  };
+
+  return result;
 }
 
 module.exports = makeRobotAccountant;
